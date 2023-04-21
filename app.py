5a58f97acb9e4c190643dc97214762670e7f9436
Pascal <pascalrai66@gmail.com>
2023-04-22 00:13:18 +0545

    structure

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template
-import numpy as np
-import pickle
-import tensorflow as tf
+from flask import render_template
 
 from flask import request
 from flask_wtf.csrf import CSRFProtect
@@ -9,26 +6,11 @@ from flask_wtf.csrf import CSRFProtect
 from wtforms import Form, TextAreaField, SubmitField, validators
 import os
 
-app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 csrf = CSRFProtect()
 csrf.init_app(app)
 
-vocab_size = 100_000
-max_len = 55
 
-model = tf.keras.models.load_model('sentiment_model.h5')
-le = pickle.load(open('le_model.sav', 'rb'))
-data = np.load('vocab.npz')
-vocab = data['arr_0']
-
-vectorize_layer = tf.keras.layers.TextVectorization(
-    max_tokens=vocab_size,
-    output_mode='int',
-    standardize='lower_and_strip_punctuation',
-    output_sequence_length=max_len,
-    vocabulary = vocab
-)
 
 class ContactForm(Form):
     
@@ -41,15 +23,9 @@ def sentiment():
     form = ContactForm(request.form)
     
     if request.method == 'POST' and form.validate():
-    
-        text_comment = form.comment.data
-        y = vectorize_layer([text_comment])
-        y = model.predict(y)
-        y = np.argmax(y,axis=1)
         
-        return f'Sentiment : {le.inverse_transform(y)[0]}'
+        return f'Sentiment : {}'
     
     return render_template('text.html', form=form)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
