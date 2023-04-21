8c980f0d19f2164f2866549495e3d850f303e2e8
Pascal <pascalrai66@gmail.com>
2023-04-21 15:54:16 +0545

    fp

--- sentiment_app.py
+++ sentiment_app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template
+import numpy as np
+import pickle
+import tensorflow as tf
 
 from flask import request
 # from flask_wtf.csrf import CSRFProtect
 
 from wtforms import Form, TextAreaField, SubmitField, validators
 
-# import os
-
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = os.urandom(24)
+
+vocab_size = 100_000
+max_len = 55
+
+model = tf.keras.models.load_model('sentiment_model.h5')
+le = pickle.load(open('le_model.sav', 'rb'))
+data = np.load('vocab.npz')
+vocab = data['arr_0']
+
+vectorize_layer = tf.keras.layers.TextVectorization(
+    max_tokens=vocab_size,
+    output_mode='int',
+    standardize='lower_and_strip_punctuation',
+    output_sequence_length=max_len,
+    vocabulary = vocab
+)
 
 class ContactForm(Form):
     
@@ -23,8 +40,11 @@ def sentiment():
     if request.method == 'POST' and form.validate():
     
         text_comment = form.comment.data
+        y = vectorize_layer([text_comment])
+        y = model.predict(y)
+        y = np.argmax(y,axis=1)
         
-        return 'Sentiment : positive'
+        return f'Sentiment : {le.inverse_transform(y)[0]}'
     
     return render_template('text.html', form=form)
 
